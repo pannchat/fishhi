a47c1053311c1226b1b6252be2b3a74c6832392a
chap95 <chap95@likelion.org>
2021-11-14 18:15:53 +0900

    add(API interceptor)

---
+++ components/info/specBox.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+interface ISpecBox {
+  name: string;
+  spec: string;
+  img?: string;
+  imgWidth?: string | number;
+  imgHeight?: string | number;
+  width?: string | number;
+  height?: string | number;
+  color?: string;
+}
+
+const SpecBox = (props: ISpecBox) => {
+  const { img, imgWidth, imgHeight, name, spec, color, width, height } = props;
+  return (
+    <div
+      className="spec-box"
+      style={{
+        width: width,
+        height: height,
+        backgroundColor: color,
+      }}
+    >
+      {img ? (
+        <img src={img} width={imgWidth} height={imgHeight} />
+      ) : (
+        <div></div>
+      )}
+
+      <div className="spec-box-footer">
+        <p className="spec-name">{name}</p>
+        <p className="spec-value">{spec}</p>
+      </div>
+
+      <style jsx>{`
+        .spec-box {
+          display: flex;
+          align-items: flex-end;
+          justify-content: space-between;
+          border-radius: 5px;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default SpecBox;
