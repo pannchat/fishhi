cf9b84275d985e7d0f3fa2a901b25a40f0a5be9f
pannchat <pannchat@likelion.org>
2021-12-04 13:12:55 +0900

    mypage

--- components/mypage/index.tsx
+++ components/mypage/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import styles from './Index.module.scss';
+import ImagePath from "../../shared/imagePath";
 const ProfileContainer = styled.div`
-    margin:20px 0;
+    margin:20px;
     width:100%;
     height:150px;
     border-radius:5px;
@@ -17,7 +19,7 @@ const ProfileInfoContainer = styled.div`
     
 `;
 const ProfileImgContainer = styled.div`
-    --ImgSize : 70px;
+    --ImgSize : 80px;
     width:var(--ImgSize);
     height:var(--ImgSize);
     overflow:hidden;
@@ -29,26 +31,83 @@ const ProfileImgContainer = styled.div`
     }
 `;
 const ProfileImg = styled.img`
-    --ImgSize : 70px;
+    --ImgSize : 80px;
     width:var(--ImgSize);
     height:var(--ImgSize);
     object-fit:cover;
 `;
 const ProfileInfo = styled.div`
-    
-
     color:white;
 `;
 const MyPage = () => {
     return(
-        <ProfileContainer>
-            <ProfileInfoContainer>
-                <ProfileImgContainer>
-                    <ProfileImg src="https://media.vlpt.us/images/duplicareus/profile/28439011-32a8-4836-b945-3a25090992bf/%EC%82%AC%EC%A7%84.jpg?w=120"/>
-                </ProfileImgContainer>
-                <ProfileInfo>닉네임</ProfileInfo>
-            </ProfileInfoContainer>
-        </ProfileContainer>
+        <div className={styles['main']}>
+            <div className={styles['profile']}>
+                <div className={styles['profile__info']}>
+                    <div className={styles['profile__img-container']}>
+                        <img className={styles['profile__img']} src="https://media.vlpt.us/images/duplicareus/profile/28439011-32a8-4836-b945-3a25090992bf/%EC%82%AC%EC%A7%84.jpg?w=120"/>
+                    </div>
+                    <div className={styles['profile__text']}>이름</div>
+                    <div className={styles['profile__text']}>이메일</div>
+                </div>
+                <div className={styles['profile__info']}>
+                    <div className={styles['profile__modified']}>
+                        프로필수정
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles['fishtank']}>
+                <div className={styles['fishtank__my-fishtank']}>
+                    <div className={styles['fishtank__my-thumb-container']}>
+                        <div className={styles['fishtank__lock']}>
+                            ㅇ    
+                        </div>
+                        <img className={styles['fishtank__my-thumb']}src={ImagePath.thumb}/>
+                    </div>
+                    <div className={styles['fishtank__my-tank-info']}>
+                        <div className={styles['fishtank__my-tank-title']}>
+                            ewfwefnewlkfnwlekfnlakmlfkmwlkfmawelkfmlakmewlkfmlkmlwkemf
+                        </div>
+                        <div className={styles['fishtank__my-tank-icon']}>
+                            ㅇ
+                        </div>
+                        <div className={styles['fishtank__my-tank-icon']}>
+                            ㅇ
+                        </div>
+                    </div>
+                </div>
+                <div className={styles['fishtank__my-fishtank']}>
+                    <div className={styles['fishtank__my-thumb-container']}>
+                        <div className={styles['fishtank__lock']}>
+                            ㅇ    
+                        </div>
+                        <img className={styles['fishtank__my-thumb']}src={ImagePath.thumb2}/>
+
+                    </div>
+                    <div className={styles['fishtank__my-tank-info']}>
+                        <div className={styles['fishtank__my-tank-title']}>
+                            ewfwefnewlkfnwlekfnlakmlfkmwlkfmawelkfmlakmewlkfmlkmlwkemf
+                        </div>
+                        <div className={styles['fishtank__my-tank-icon']}>
+                            ㅇ
+                        </div>
+                        <div className={styles['fishtank__my-tank-icon']}>
+                            ㅇ
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        // <ProfileContainer>
+        //     <ProfileInfoContainer>
+        //         <ProfileImgContainer>
+        //             <ProfileImg src="https://media.vlpt.us/images/duplicareus/profile/28439011-32a8-4836-b945-3a25090992bf/%EC%82%AC%EC%A7%84.jpg?w=120"/>
+        //         </ProfileImgContainer>
+        //         <ProfileInfo>닉네임</ProfileInfo>
+        //         <ProfileInfo>닉네임</ProfileInfo>
+        //     </ProfileInfoContainer>
+        // </ProfileContainer>
     );
 }
 
