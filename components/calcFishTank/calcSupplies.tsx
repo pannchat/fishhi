48869c11d04ec70a13ce63350c3405f79c133e2d
chap95 <chap95@likelion.org>
2022-01-12 23:03:20 +0900

    add commit

--- components/calcFishTank/calcSupplies.tsx
+++ components/calcFishTank/calcSupplies.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { useSuppliesProduct } from '../../shared/hooks/useSuppliesProduct';
-import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
+import { BsBookmark, BsBookmarkFill } from 'react-icons/bs';
 
 const IMAGE_BASE_URL = 'https://fishhi.kr/thumbnails/';
 const CalcSupplies = () => {
   const { data } = useSuppliesProduct();
   const [toggle, setToggle] = useState(true);
-
+  // commit
   if (!data) return <div></div>;
   return (
     <div className="calc-supplies">
@@ -15,7 +15,7 @@ const CalcSupplies = () => {
         const { img, productName, recommendedUsage1, recommendedUsage2 } = value;
         return (
           <li className="search-list" key={`searchList${index}`}>
-            <div className='supplies-items'>
+            <div className="supplies-items">
               <img
                 src={`https://fishhi.kr/thumbnails/${img}`}
                 alt={`${productName} 상품`}
@@ -34,7 +34,7 @@ const CalcSupplies = () => {
                 </p>
                 <p>
                   내 어항 사용량 :{' '}
-                  <b style={{color:'#8aa1a1'}}>
+                  <b style={{ color: '#8aa1a1' }}>
                     {/* {capacity
                     ? `${(
                         (capacity / recommendedUsage1) *
@@ -46,13 +46,18 @@ const CalcSupplies = () => {
                   권장
                 </p>
               </div>
-              <div className='test3'>
-                {toggle 
-                  ? <BsBookmark size="25" color="#8aa1a1" style={{cursor:'pointer'}} onClick={()=>setToggle(true)}/>
-                  : <BsBookmarkFill size="25" color="#8aa1a1" style={{cursor:'pointer'}} onClick={()=>setToggle(false)}/>
-                }
+              <div className="test3">
+                {toggle ? (
+                  <BsBookmark size="25" color="#8aa1a1" style={{ cursor: 'pointer' }} onClick={() => setToggle(true)} />
+                ) : (
+                  <BsBookmarkFill
+                    size="25"
+                    color="#8aa1a1"
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => setToggle(false)}
+                  />
+                )}
               </div>
-              
             </div>
           </li>
         );
@@ -62,30 +67,29 @@ const CalcSupplies = () => {
         .search-list {
           display: flex;
         }
-        .supplies-items{
-          display:flex;
-          width:100%;
+        .supplies-items {
+          display: flex;
+          width: 100%;
           border-radius: 5px;
           margin: 15px 5px;
           box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
         }
-        .test2{
-          display:flex;
+        .test2 {
+          display: flex;
           flex-direction: column;
-          padding:10px 0;
-          flex:1 0;
+          padding: 10px 0;
+          flex: 1 0;
         }
-        .supply-product-name{
+        .supply-product-name {
           font-weight: bold;
-          font-size:1.5em;
-          margin-bottom:auto;
+          font-size: 1.5em;
+          margin-bottom: auto;
         }
-        .test3{
-          display:flex;
-          justify-content:center;
-          flex-basis:50px;
+        .test3 {
+          display: flex;
+          justify-content: center;
+          flex-basis: 50px;
         }
-
       `}</style>
     </div>
   );
