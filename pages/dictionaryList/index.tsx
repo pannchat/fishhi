a30856796c09f153d2884bae771d4049deeeadf3
jiwon <parkjiwon@Park-Jiwon.local>
2022-02-27 14:57:41 +0900

    dictionaryList

---
+++ pages/dictionaryList/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import DictionaryList from "../../components/dictionaryList";
+
+
+const DictionaryListPage = () => {
+  return <DictionaryList />;
+};
+
+export default DictionaryListPage;
