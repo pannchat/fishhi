e178ef3da0cd31781660a6298f47de0eef0673ae
jiwon <parkjiwon@Park-Jiwon.local>
2022-03-15 08:48:44 +0900

    fishlist

--- components/dictionaryList/index.tsx
+++ components/dictionaryList/index.tsx
@@ -1,69 +1,92 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-    Button, Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    Image,
-    MenuItemOption,
-    MenuGroup,
-    MenuOptionGroup,
-    MenuDivider,
-    useToast, Slider, SliderTrack, SliderFilledTrack, SliderThumb, SliderMark
-} from "@chakra-ui/react";
-import { FiChevronDown } from 'react-icons/fi';
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    Button,
+} from '@chakra-ui/react';
 
-enum categoryType  {
+import { FiChevronDown } from 'react-icons/fi';
+import Category from './Category';
+import axios from 'axios';
+
+export enum categoryType {
     'default' = '옵션',
     'fish' = '물고기',
     'aquaplant' = '수초',
     'supplies' = '용품'
 }
-export default function dictionaryList() {
-    const [category, setCategory] = useState<string>(categoryType.default)
-    const clickMenuItem = (item : string) => {
-        setCategory(item)
-    }
-    return (
-        <div>
-            <Menu>
-                <MenuButton as={Button} rightIcon={<FiChevronDown />}>
-                    {category}
-                </MenuButton>
-                <MenuList>
-                    <MenuItem id={categoryType.fish} minH='40px' onClick={(e)=>clickMenuItem(categoryType.fish)} >
-                        <Image
-                            boxSize='2rem'
-                            borderRadius='full'
-                            src='https://images.unsplash.com/photo-1534043464124-3be32fe000c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=706&q=80'
-                            alt='Simon the pensive'
-                            mr='12px'
-                        />
-                        <span>{categoryType.fish}</span>
-                    </MenuItem>
 
-                    <MenuItem minH='48px' onClick={(e)=>clickMenuItem(categoryType.aquaplant)}>
-                        <Image
-                            boxSize='2rem'
-                            borderRadius='full'
-                            src='https://images.unsplash.com/photo-1579967327980-2a4117da0e4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1149&q=80'
-                            alt='Fluffybuns the destroyer'
-                            mr='12px'
-                        />
-                        <span>{categoryType.aquaplant}</span>
-                    </MenuItem>
-                    <MenuItem minH='40px' onClick={(e)=>clickMenuItem(categoryType.supplies)}>
-                        <Image
-                            boxSize='2rem'
-                            borderRadius='full'
-                            src='https://ae01.alicdn.com/kf/H5e5c31a2764b4030a4b3a6308bdb3cbbv/Viv-ada.jpg_Q90.jpg_.webp'
-                            alt='Simon the pensive'
-                            mr='12px'
-                        />
-                        <span>{categoryType.supplies}</span>
-                    </MenuItem>
-                </MenuList>
-            </Menu>
+
+
+export default function dictionaryList() {
+    const [category, setCategory] = useState<string>(categoryType.default);
+    const [listData, setListData] = useState<[]>([]);
+    const clickMenuItem = (item: string) => {
+        setCategory(item);
+    };
+    const getPost = async () => {
+        const res = await axios.get('http://54.180.156.194:8000/fish/?offset=0&limit=100');
+        setListData(res.data.results);
+    };
+
+    const deletePost = async (id: number) => {
+        const res = await axios.delete(`http://54.180.156.194:8000/fish/${id}`);
+        console.log(res);
+    };
+    const deleteList = () => {
+        switch (category) {
+            case categoryType.fish:
+
+        }
+    };
+
+    useEffect(() => {
+        console.log(getPost());
+    }, []);
+    return (
+
+        <div>
+            <Category />
+            <Table variant='simple'>
+                <TableCaption>Imperial to metric conversion factors</TableCaption>
+                <Thead>
+                    <Tr>
+                        <Th width={'15px'} isNumeric>id</Th>
+                        <Th >어종</Th>
+                        <Th >이미지</Th>
+                        <Th >관리</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {listData.map((post: any) => {
+                        return (
+                            <Tr>
+                                <Td isNumeric>{post.id}</Td>
+                                <Td>{post.species}</Td>
+                                <Td ><img style={{ width: '80px' }} src={post.thumbnail} /></Td>
+                                <Td >
+                                    <Button colorScheme='teal' size='xs' onClick={() => deletePost(post.id)}>삭제</Button>
+                                </Td>
+                            </Tr>
+                        );
+                    })}
+
+
+                </Tbody>
+                {/* <Tfoot>
+                    <Tr>
+                        <Th>To convert</Th>
+                        <Th>into</Th>
+                        <Th isNumeric>multiply by</Th>
+                    </Tr>
+                </Tfoot> */}
+            </Table>
         </div>
     );
 }
