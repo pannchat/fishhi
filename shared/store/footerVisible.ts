813b544ec939e04f9dabc9410c398b4b7979b961
chap95 <chap95@likelion.org>
2022-03-28 19:46:14 +0900

    fix(error)

--- shared/store/footerVisible.ts
+++ shared/store/footerVisible.ts
@@ -1,6 +1,6 @@
-import { atom } from 'recoil';
+import { atom } from "recoil";
 
 export const footerVisibleState = atom<boolean>({
-  key: 'isFooterVisible',
+  key: "footerVisibleState",
   default: true,
 });
